a18d1335fc8ff813ce67f3463a0303a8176a53ab
kitikil2 <kitikilabdelaziz@gmail.com>
2018-12-31 17:15:01 +0100

    Signed-off-by: kitikil2 <kitikilabdelaziz@gmail.com>

---
+++ workspace212/gestionciuniv/src/main/java/org/sled/gestionciuniv/App.java
@@ -0,0 +1,13 @@
+package org.sled.gestionciuniv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ workspace212/gestionciuniv/src/test/java/org/sled/gestionciuniv/AppTest.java
@@ -0,0 +1,20 @@
+package org.sled.gestionciuniv;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
